746437d4921ca04b2efc2e520cdadbbe63bc5879
S MOHAMMAD ASHRAF <ashrafali31033@gmail.com>
2024-12-03 11:04:11 +0530

    Add files via upload

---
+++ ashraf5.java
@@ -0,0 +1,17 @@
+public class ashraf5 {
+    public static void main(String args[])
+    {
+        int  boy =50;
+        int girl =20;
+        int c=boy;
+
+        boy=girl;
+        girl=c;
+
+        System.out.println(boy);
+        System.out.println(girl);
+
+
+    }
+    
+}
