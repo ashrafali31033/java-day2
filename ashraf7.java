1ad8f573d3d7ba0a463f2cf3a5a84ef3fc0c85db
S MOHAMMAD ASHRAF <ashrafali31033@gmail.com>
2024-12-03 14:59:18 +0530

    Add files via upload

---
+++ ashraf7.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class ashraf7 {
+    public static void main(String args[])
+    {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter 1 no :");
+        int a = sc.nextInt();
+        System.out.println("enter 2 no :");
+        int b = sc.nextInt();
+        
+        int c=a^(1<<b);
+        
+
+
+
+
+        System.out.println("the output is :"+c);
+
+    }
+    
+}
