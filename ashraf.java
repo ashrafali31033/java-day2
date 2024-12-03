14adc4ce28b1205c311ea0e11fd59c02320455f6
ashrafali31033 <ashrafali31033@gmail.com>
2024-12-03 10:22:31 +0530

    Add files via upload

---
+++ ashraf.java
@@ -0,0 +1,16 @@
+public class ashraf {
+    public static void main(String args[])
+    {
+        int a=12;
+        int b=20;
+
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a<=b);
+        System.out.println(a>=b);
+        System.out.println(a>b);
+        System.out.println(a<b);
+
+    }
+    
+}
